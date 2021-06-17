6482b85edf498a5826341cec7562fc45f18ff0c0
MarianaRojas13 <marianarojasoyarce@gmail.com>
2021-06-16 23:26:40 -0400

    clase 10/06

--- web/game/resources/views/layouts/master.blade.php
+++ web/game/resources/views/layouts/master.blade.php
@@ -1,11 +1,11 @@
-/*padazos de otras paginas*/
+<!--padazos de otras paginas-->
 <!doctype html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="csrf-token"content="{{csrf_token()}}"> <!--sirve para que no exploten las peticiones al servidor-->
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
@@ -13,11 +13,11 @@
   </head>
   <body>
     <header class="container-fluid" > <!--Esto del primary son colores ya definidos ejemplo bg-warning es amarillo-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#"><img src="{{asset('img/Dragon_Ball_Z.png')}}" class="img-fluid" class="logo"></a>
+        <nav class="navbar navbar-expand-lg navbar-light bg-warning" >
+            <div class="container-fluid"><!--asset con  permite acceder a la carpeta public,y todas las carpetas en ella-->
+              <a class="navbar-brand" href="#"><img src="{{asset('img/Dragon_Ball_Z.png')}}" class="logo"></a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon" ></span>
               </button>
               <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav"><!--disabled es para bloquear, ver juegs lo tenia, active lo marca mas-->
@@ -37,7 +37,8 @@
 
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="{{asset('js/axios_config.js')}}"></script>
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
